677e4c79e265e05bfa9672076e36e4b205a6e5f2
dima <030103dim@gmail.com>
2024-10-30 13:46:38 +0200

    create class ShoppingCartTest

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -53,18 +53,23 @@ public class ShoppingCart{
      * @throws IllegalArgumentException if some value is wrong 
      */
     public void addItem(String title, double price, int quantity, ItemType type){
+
         if (title == null || title.length() == 0 || title.length() > 32)
             throw new IllegalArgumentException("Illegal title");
+
         if (price < 0.01)
             throw new IllegalArgumentException("Illegal price");
+
         if (quantity <= 0)
             throw new IllegalArgumentException("Illegal quantity");
+
         Item item = new Item();
-        item.title  = title;
-        item.price  = price;
-        item.quantity = quantity;
-        item.type = type;
+        item.setTitle(title);
+        item.setTotalPrice(price);
+        item.setQuantity(quantity);
+        item.setItemType(type);
         items.add(item);
+
     }
 
     /**
